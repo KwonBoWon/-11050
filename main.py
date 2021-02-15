8638f83ce53a6a1c93e2343ece8b8a365ff2fca8
KwonBoWon <bogod1207@naver.com>
2021-02-15 14:45:33 +0000

    memoization dynamic

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import sys
 input=sys.stdin.readline
 
-def f(N):
-  if N<=1:
-    return 1
-  else: return (N*f(N-1))
+def f(n,k):
+  if memo[n][k]>0:
+    return memo[n][k]
+  if n==k or k==0:
+    memo[n][k]=1
+    return memo[n][k]
+  memo[n][k]=f(n-1, k-1)+ f(n-1, k)
+  return memo[n][k]
 
 
 N, K=map(int, input().split())
-
-
-print(round(f(N)/(f(K)*f(N-K))))
+memo=[[-1 for _ in range(N+1)] for _ in range(N+1)]
+print(f(N,K))
